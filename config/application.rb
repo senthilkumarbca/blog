226a3d56384cabffa9d0c28240c2b4bc1ddff210
unknown <senthilkumarbca>
2022-11-23 17:27:20 +0530

    production

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module BlogS
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.secret_key_base = 'blipblapblup'
   end
 end
