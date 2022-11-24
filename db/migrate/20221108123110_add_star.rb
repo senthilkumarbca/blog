53e5c2f7eb414befaa1592c8daf03ead1d4dd65d
unknown <senthilkumarbca>
2022-11-24 17:08:12 +0530

    db2

--- db/migrate/20221108123110_add_star.rb
+++
@@ -1,5 +0,0 @@
-class AddStar < ActiveRecord::Migration[7.0]
-  def change
-    add_column :user_comment_ratings, :star, :integer
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-20.times do |i|
-  Post.create(title: "post #{i+1}",content:"this is the post and it is the #{i+1}th post",topic_id:"#{rand(10..13)}")
-end
